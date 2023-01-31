f60f8ba1fef6ae3e3e5adfdeb6695ccad1b91d4e
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-01-31 11:03:41 +0530

    Fifth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,38 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Coordinates implements Cloneable {
+    private static final Logger LOGGER = Logger.getLogger("global");
+    int x;
+    int y;
+
+    Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public String equals(Coordinates c3) {
+        String result;
+        result = this.x == c3.x && this.y == c3.y ? "true" : "false";
+        return result;
+    }
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+            Scanner sc = new Scanner(System.in);
+            LOGGER.log(Level.INFO, "Enter the value x:");
+            int x = sc.nextInt();
+            LOGGER.log(Level.INFO, "Enter the value y:");
+            int y = sc.nextInt();
+            Coordinates c = new Coordinates(x, y);
+            Coordinates c2 = (Coordinates) c.clone();
+            String s = c.equals(c2);
+            LOGGER.info("Orginal object and clonable object is Same ?: ");
+            LOGGER.info(s);
     }
 }
